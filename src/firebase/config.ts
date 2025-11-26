091c0ef97b7f686288b5679043d82c982fd2ca69
Lucas Teodoro <lucas.coryy@gmail.com>
2025-11-26 23:12:30 +0000

    Credenciais do firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,11 +2,11 @@
 // The Firebase configuration will be populated by the backend.
 // You can find your web app's Firebase configuration in the
 // Firebase console.
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyBRwb2GCmmg1p7_879FX_fWP7Axk61kGEk",
+  authDomain: "studio-4950713027-a8370.firebaseapp.com",
+  projectId: "studio-4950713027-a8370",
+  storageBucket: "studio-4950713027-a8370.firebasestorage.app",
+  messagingSenderId: "661279768972",
+  appId: "1:661279768972:web:95846e09a208ced2cb56a9"
 };
