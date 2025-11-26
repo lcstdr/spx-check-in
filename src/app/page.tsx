dda3016d039495abc1a95f686a82ca1f5f7e3431
Lucas Teodoro <lucas.coryy@gmail.com>
2025-11-26 23:00:44 +0000

    Tela Inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -163,7 +163,7 @@ export default function CheckInPage() {
           SPX CHECK-IN MAUÁ
         </h1>
         <p className="text-muted-foreground mt-2 max-w-md">
-          MAUÁ LSP64 - Check-in automático por geolocalização para membros da equipe.
+          AJUSTE - MAUÁ LSP64 - Check-in automático por geolocalização para membros da equipe.
         </p>
       </div>
 
